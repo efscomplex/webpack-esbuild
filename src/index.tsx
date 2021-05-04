675609d86bc7b0a05904d5ecee45ec30e89b18cf
EasyDev <35841638+efscomplex@users.noreply.github.com>
2021-05-04 11:07:57 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import 'styles/index.css'
-const API_KEY = process.env.GOOGLE_API_KEY
-console.log(API_KEY)
 
 const App = () => <h1>'hello world!'</h1>
 render(<App />, document.getElementById('app'))
